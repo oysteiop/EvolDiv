571df3d6c15e236fd03314930b911a08e3486a5a
Oystein Opedal <oystein.opedal@helsinki.fi>
2019-05-02 12:59:22 +0300

    Added Crepis analyses

--- analyses/andersson_Crepis_analysis.R
+++ analyses/andersson_Crepis_analysis.R
@@ -4,6 +4,7 @@
 rm(list=ls())
 library(evolvability)
 library(plyr)
+library(reshape2)
 library(MCMCglmm)
 library(lme4)
 library(kinship2)
@@ -16,7 +17,6 @@ list.files(path="./data/andersson")
 dat = read.csv2("./data/andersson/Crepis_leaf_data.csv", dec=".")
 head(dat)
 str(dat)
-
 
 #####################################
 #### - Estimating the G matrix - ####
@@ -41,19 +41,26 @@ head(pedigree)
 
 #Mean-scale and multiply by 10
 #names(dat)[3]="animal"
-#dat[,c(4:8)]=apply(dat[,c(4:8)], 2, function(x) 10*x/mean(x, na.rm=T))
+dat[,c(4:8)]=apply(dat[,c(4:8)], 2, function(x) 10*x/mean(x, na.rm=T))
 head(dat)
 
 #Simple analysis
 m=lmer(LEN~1+(1|IDNO),data=dat)
 summary(m)
+.568*2
 (20.29*2)/63.13^2*100
 
 m=lmer(MAX~1+(1|IDNO),data=dat)
 summary(m)
+.6205*2
+
+m=lmer(MIN~1+(1|IDNO),data=dat)
+summary(m)
+.6205*2
+
 
 #Run the MCMCglmm analysis
-Aped <- 2 * kinship2::kinship(pedigree[, 1], pedigree[,2], pedigree[, 3])
+#Aped <- 2 * kinship2::kinship(pedigree[, 1], pedigree[,2], pedigree[, 3])
 
 invA = inverseA(pedigree)$Ainv
 
@@ -82,7 +89,7 @@ save(mod, file="./analyses/andersson_crepis/Gmat75k.RData")
 
 #Check convergence
 summary(mod$VCV)
-plot(mod$VCV[,1])
+plot(mod$VCV[,19])
 
 #Compile G matrix
 gmat=matrix(apply(mod$VCV, 2, median)[1:(n*n)], nrow=n)
@@ -97,4 +104,70 @@ evolvabilityMeans(msG)
 #####################################
 #### - Estimating the D matrix - ####
 #####################################
+dat = read.csv2("./data/andersson/Crepis_leaf_data.csv", dec=".")
+dat=dat[dat$TYPE=="POP",]
+head(dat)
 
+popstats=ddply(dat, .(IDENTITY),summarize,
+               n=length(IDNO),
+               LENm=mean(LEN,na.rm=T),
+               LENsd=sd(LEN,na.rm=T),
+               TIPm=mean(TIP,na.rm=T),
+               TIPsd=sd(TIP,na.rm=T),
+               MAXm=mean(MAX,na.rm=T),
+               MAXsd=sd(MAX,na.rm=T),
+               MINm=mean(MIN,na.rm=T),
+               MINsd=sd(MIN,na.rm=T),
+               TEETHm=mean(TEETH,na.rm=T),
+               TEETHsd=sd(TEETH,na.rm=T))
+popstats
+
+
+dat[,c(4:8)]=apply(dat[,c(4:8)], 2, function(x) log(x))
+head(dat)
+
+#Simple analysis
+m=lmer(LEN~1+(1|IDENTITY),data=dat)
+summary(m)
+
+m=lmer(MAX~1+(1|IDNO),data=dat)
+summary(m)
+
+m=lmer(MIN~1+(1|IDNO),data=dat)
+summary(m)
+
+
+#Five traits
+n = 5
+alpha.mu <- rep(0, n)
+alpha.V <- diag(n)*5
+prior<-list(R=list(V=diag(n), nu=n+0.002-1), 
+            G=list(G1=list(V=diag(n), nu=n, alpha.mu = alpha.mu, alpha.V = alpha.V)))
+
+samples = 1000
+thin = 50
+burnin = samples*thin*.5
+nitt = (samples*thin)+burnin
+
+a=Sys.time()
+mod<-MCMCglmm(c(LEN,TIP,MAX,MIN,TEETH) ~ -1+trait,
+              random = ~us(trait):IDENTITY,
+              rcov = ~us(trait):units,
+              data = dat,
+              family = rep("gaussian", n), prior = prior, 
+              nitt = nitt, burnin = burnin, thin = thin)
+Sys.time()-a
+
+save(mod, file="./analyses/andersson_crepis/Dmat75k.RData")
+
+#Check convergence
+summary(mod$VCV)
+plot(mod$VCV[,19])
+
+#Compile D matrix
+dmat=matrix(apply(mod$VCV, 2, median)[1:(n*n)], nrow=n)
+colnames(dmat)=rownames(dmat)=colnames(dat)[4:8]
+dmat*100
+
+gmat
+plot(log(diag(gmat)), log(diag(dmat*100)))
